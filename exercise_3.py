9b8eb8ad45224af63736e55c2290c7263e17ccde
fereneyb-stack <fereneyb@msmk.university>
2026-02-27 16:19:06 -0500

    ferney_prito

--- exercise_3.py
+++ exercise_3.py
@@ -1,2 +1,9 @@
 # 3. Crea una lista llamada `colors` con los siguientes colores: `red`, `green`, `blue`, `yellow`, y `purple`.
 # Luego imprime usando un bucle `for` cada color en la lista.
+from turtle import color
+
+
+colors = ["red","green","blue","yellow","purple"]
+for color in colors:
+    print(color)
+    

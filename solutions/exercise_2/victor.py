fe34a6343c69fa58b23109bc709586d97462554a
victor <victoref@msmk.university>
2026-03-05 09:54:33 +0100

    create diccionary

---
+++ solutions/exercise_2/victor.py
@@ -0,0 +1,4 @@
+# 2. Crea un diccionario llamado `person` con las siguientes claves: `name`, `age`, y `city`. 
+# Asigna valores apropiados a cada clave.
+person = { "name": "victor", "age": 19, "city": "Madrid"}
+print(person)

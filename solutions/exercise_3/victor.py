d4dc58d22984ed59b80984bec8db961f8ae9e220
victor <victoref@msmk.university>
2026-03-05 10:06:59 +0100

    create color list

---
+++ solutions/exercise_3/victor.py
@@ -0,0 +1,5 @@
+# 3. Crea una lista llamada `colors` con los siguientes colores: `red`, `green`, `blue`, `yellow`, y `purple`.
+# Luego imprime usando un bucle `for` cada color en la lista.
+colors= [ "reden", "green", "blue", "yellow", "purple" ]
+for color in colors:
+    print(color)

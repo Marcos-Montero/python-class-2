fde2c3e0ca61871acd47a14520ad60a874c5b8a8
carlos_lamas <lamas.carlos.a.maux@gmail.com>
2026-03-22 07:07:37 -0400

    to exercise 2

---
+++ solutions/excercise_2/Carlos.py
@@ -0,0 +1,10 @@
+# 2. Crea un diccionario llamado `person` con las siguientes claves: `name`, `age`, y `city`. 
+# Asigna valores apropiados a cada clave.
+person={
+    "name":"carlos",
+    "age":30,
+    "city":"Barcelona"
+}
+   
+   
+
